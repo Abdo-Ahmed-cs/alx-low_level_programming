a9ac702f823b8b0038055dcf190eaeb4326d88fd
abdelrahman ahmed saad <abdelrahman9789@gmail.com>
2023-02-16 12:07:02 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'this program prints the size of every data type'
+ *
+ * Return: return 0 and exit the program 
+ */
+
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+	printf("Size of a char: %d\n",sizeof(a))
+	printf("Size of a int: %d\n",,sizeof(b))
+	printf("Size of a long int: %d\n",sizeof(c))
+	printf("Size of a long long int: %d\n",sizeof(d))
+	printf("Size of a float: %d\n",sizeof(e))
+	return (0);
+}
