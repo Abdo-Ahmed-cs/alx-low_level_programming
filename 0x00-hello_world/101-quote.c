fe2acbf9ea554a2bba6c7f1298d234bcb7e26c40
abdelrahman ahmed saad <abdelrahman9789@gmail.com>
2023-02-16 12:33:10 -0800

    8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: 'this program prints a scentence without using printf or puts'
+ *
+ * Return: return 0 and exits the program
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\"
+		- Dora Korpar, 2015-10-19");
+	return (0);
+}
