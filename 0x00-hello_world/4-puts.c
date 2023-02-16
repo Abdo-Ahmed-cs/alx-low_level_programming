2c68ad65a346e8e5470727ac6b3d5e2ef29ccfcc
abdelrahman ahmed saad <abdelrahman9789@gmail.com>
2023-02-16 11:47:16 -0800

    hello puts second try

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: 'this program prints a scentence to the standard output'
+ *
+ * Return: retun 0 and exit the program
+ */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
