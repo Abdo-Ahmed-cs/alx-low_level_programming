7c716c7818c3d23bef234634346040c201ef5ae5
abdelrahman ahmed saad <abdelrahman9789@gmail.com>
2023-02-17 08:15:40 -0800

    0 positive negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - entry point
+ *Description: 'this program compares a random number with 0'
+ *Return: return 0 and exit the program
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+that is a directory me file about if, ifelse, and loos in c
