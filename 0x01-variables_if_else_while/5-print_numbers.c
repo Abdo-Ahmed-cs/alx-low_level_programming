1b81d65b417bf57ba7b0548e8739a688d45cd64f
abdelrahman ahmed saad <abdelrahman9789@gmail.com>
2023-02-17 10:14:36 -0800

    5 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: 'this program prints the alphapit in lower case'
+ *
+ * Return: return 0 and exit
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
