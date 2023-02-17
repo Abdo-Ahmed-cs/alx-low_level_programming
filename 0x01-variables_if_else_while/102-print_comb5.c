5a2a5999e01e7a7c5d055d310d6401f9cf129478
abdelrahman ahmed saad <abdelrahman9789@gmail.com>
2023-02-17 15:16:17 -0800

    12 software is eating the world

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: 'this program prints compination of numbers in 3 pairs'
+ *
+ * Return: return 0 and exit
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 9; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+				for (l = 0; l < 10; l++)
+				{
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar(' ');
+					putchar('0' + k);
+					putchar('0' + l);
+					if (!(i == 9 && j == 8 && k == 9 && l == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
