a53b03b8854b0f82e561b58584a7b5cf5d1bc05d
abdelrahman ahmed saad <abdelrahman9789@gmail.com>
2023-02-21 12:28:16 -0800

    8-24_hours.c second try

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,21 +15,21 @@ void jack_bauer(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if(i == 2 && j == 4)
+			if (i == 2 && j == 4)
 			{
 				break;
 			}
 			for (k = 0; k < 6; k++)
 			{
-				for(l = 0; l < 10; l++)
+				for (l = 0; l < 10; l++)
 				{
 					_putchar(i + '0');
 					_putchar(j + '0');
 					_putchar(':');
 					_putchar(k + '0');
 					_putchar(l + '0');
+					_putchar('\n');
 				}
-				_putchar('\n');
 			}
 		}
 	}
