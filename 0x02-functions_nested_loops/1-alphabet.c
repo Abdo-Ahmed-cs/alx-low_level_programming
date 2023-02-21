98fa6aff4da354b1d2fc8a0f9491823e0a5855e3
abdelrahman ahmed saad <abdelrahman9789@gmail.com>
2023-02-21 10:56:38 -0800

    1- alphabet.c fifth try

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,6 +2,23 @@
 #include "main.h"
 
 /**
+ * print_alphabet - prints alphabet
+ */
+
+void print_alphabet(void)
+{
+	char ch_l = 'a';
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		_putchar(ch_l);
+		ch_l++;
+	}
+	_putchar('\n');
+}
+
+/**
  * main - entry point
  *
  * Description: 'this program use _putchar to print the alphapet'
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,19 +1,3 @@
-#ifndef MAIN_H_
-#define MAIN_H_
-
 int _putchar(char c);
 
-void print_alphabet(void)
-{
-        char ch_l = 'a';
-        int i;
-
-        for (i = 0; i < 26; i++)
-        {
-                _putchar(ch_l);
-                ch_l++;
-        }
-        _putchar('\n');
-}
-
-#endif
+void print_alphabet(void);
