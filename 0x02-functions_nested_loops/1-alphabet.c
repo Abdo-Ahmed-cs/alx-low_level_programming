ad837bc109fa0bee816b463aaf64750c001bdd53
abdelrahman ahmed saad <abdelrahman9789@gmail.com>
2023-02-21 10:40:50 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet
+ */
+
+void print_alphabet(void)
+{
+	char ch_l = 'a';
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		_putcahr(ch_l);
+		ch_l++;
+	}
+	_putchar('\n');
+}
+
+/**
+ * main - entry point
+ *
+ * Description: 'this program use _putchar to print the alphapet'
+ *
+ * Return: return 0 and exit the program
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
