3daefde403c5b0578048f439aebb27c5f559e5eb
abdelrahman ahmed saad <abdelrahman9789@gmail.com>
2023-02-21 15:16:29 -0800

    103-fib.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: 'sum of even fib numbers less than 400000'
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	long fib1 = 1;
+	long fib2 = 2;
+	long fib3;
+	long sum = 2;
+
+	while (fib3 < 4000000)
+	{
+		fib3 = fib1 + fib2;
+		fib1 = fib2;
+		fib2 = fib3;
+
+		if (fib3 % 2 == 0)
+		{
+			sum += fib3
+		}
+	}
+
+	printf("%li", sum);
+	return (0);
+}
