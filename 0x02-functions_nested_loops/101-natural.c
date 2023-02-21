eb107362c1b0cb0d8a2d7471bdc7742c87dbf6b2
abdelrahman ahmed saad <abdelrahman9789@gmail.com>
2023-02-21 14:46:44 -0800

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: 'this program prints numbers that can be divided by 5 of 3'
+ *
+ * Return: return 0 and exit the program
+ */
+
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+
+}
