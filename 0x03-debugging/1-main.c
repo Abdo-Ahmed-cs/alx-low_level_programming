9949fe017b054adc8ff9eb3461c80d24bee3b35f
abdelrahman ahmed saad <abdelrahman9789@gmail.com>
2023-02-22 11:45:02 -0800

    1 like , comment, subscribe

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - test a file to avoide infinite loop
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+	/**
+	* while (i < 10)
+	* {
+	*	putchar(i);
+	*}
+	*/
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
+}
