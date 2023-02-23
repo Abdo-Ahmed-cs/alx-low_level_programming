62d1833d68f0603f6722850c9724b2162f53108a
abdelrahman ahmed saad <abdelrahman9789@gmail.com>
2023-02-23 15:28:25 -0800

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,32 @@
+#include <stido.h>
+
+/**
+ * main - entry point
+ *
+ * Description: 'the largest prime factor of choosen number'
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	long int num = 612852475143;
+	int div = 2;
+
+	while (1)
+	{
+		if (num / div == 1 && num % div == 0)
+		{
+			printf("%d\n", div);
+			break;
+		}
+		if (num % div == 0)
+		{
+			num = num / div;
+		} else
+		{
+			div++;
+		}
+	}
+	return (0);
+}
