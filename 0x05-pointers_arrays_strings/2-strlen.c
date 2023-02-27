bd3e87e0bd55acf6ca569e8e93852b85ea82fbfa
abdelrahman ahmed saad <abdelrahman9789@gmail.com>
2023-02-27 13:49:26 -0800

    2 strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - gives the length of a string
+ * @s: a pointer to the array of characters
+ *
+ * Return: return the lenght of str
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
