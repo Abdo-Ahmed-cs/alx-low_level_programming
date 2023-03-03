c7b940949e16a9fe0bf3afc99532af40ee838d9f
abdelrahman ahmed saad <abdelrahman9789@gmail.com>
2023-03-02 18:33:45 -0800

    6 fourth try

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,10 +11,10 @@ char *cap_string(char *n)
 {
 	int i = 0;
 
-	while (n[i]  != '\0')
+	while (n[i])
 	{
 		while (!(n[i] >= 'a' && n[i] <= 'z'))
-			i++;
+				i++;
 
 			if (n[i - 1] == ' ' ||
 			n[i - 1] == '\t' ||
