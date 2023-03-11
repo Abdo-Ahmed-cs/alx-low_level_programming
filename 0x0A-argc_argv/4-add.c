52ca493be7e13b5bf3f1c8b41810daf9ac1b4897
abdelrahman ahmed saad <abdelrahman9789@gmail.com>
2023-03-10 17:16:11 -0800

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * check - checking from integer
+ * @s: string pointer
+ *
+ * Return: 1 if integer 0 if not
+ */
+
+int check(char *s)
+{
+	int count = 0;
+
+	while(count < strlen(s))
+	{
+		if (!isdigit(s[count]))
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
+}
+
+
+/**
+ * main - entry point
+ *
+ * Description: 'program that adds positive numbers'
+ *
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: return the multi value
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+	
+	for (i = 1; i < argc; i++)
+	{
+		int num = atoi(argv[i]);
+
+		if (check(argv[i]))
+		{
+			sum = sum + num;
+		} else
+		{
+			printf("Error\n");
+			return (1);
+			break;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
