3c55ef79a10cbdea8be24f751ed16f5659ecd193
abdelrahman ahmed saad <abdelrahman9789@gmail.com>
2023-03-14 14:02:48 -0700

    2. He who is not courageous enough to take risks will accomplish nothing in life 4

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,11 +17,11 @@ char *str_concat(char *s1, char *s2)
 	int l;
 	char *s;
 
-	while (s1[i] != '\0' && *s1 != "")
+	while (s1[i] != '\0' && s1)
 	{
 		i++;
 	}
-	while (s2[j] != '\0' && *s2 != "")
+	while (s2[j] != '\0' && s2)
 	{
 		j++;
 	}
