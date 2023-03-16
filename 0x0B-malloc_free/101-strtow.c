21b7591838ef66c48d76ed1779d4774df0de3e3a
abdelrahman ahmed saad <abdelrahman9789@gmail.com>
2023-03-15 18:16:31 -0700

    101

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * **strtow - splits a string into words
+ * @str: string pointer
+ *
+ * Return: null on faliur otherwise return pointer of words
+ */
+
+char **strtow(char *str)
+{
+	int i, j;
+	int k = 0;
+	int wlength = 1;
+	int words = 0;
+	int r = 0;
+	char **s;
+
+
+	if (str == NULL || str == "")
+	{
+		return (NULL);
+	}
+	while (*str)
+	{
+		wlength++;
+		if (str[k] == ' ' && str[k + 1] != ' ')
+		{
+			words++;
+		}
+		if (str[k] != ' ')
+		{
+			k++;
+		}
+	}
+	str = (char **) malloc(sizeof(*char) words);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < words)
+	{
+		for (j = 0; j < wlength; j++)
+		{
+			s[i][j] = str[r];
+			r++;
+		}
+		s[i][r++] = '\0';
+	}
+	return (s);
+}
