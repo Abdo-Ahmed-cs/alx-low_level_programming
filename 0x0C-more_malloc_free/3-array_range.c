730b60243f8dba8b9254d5defab050ccaf329e0b
abdelrahman ahmed saad <abdelrahman9789@gmail.com>
2023-03-16 09:21:48 -0700

    3 array_range 2

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -21,11 +21,14 @@ int *array_range(int min, int max)
 	}
 
 	p = malloc(sizeof(int) * (max - min + 1));
-
+	if (p == NULL)
+	{
+		return (NULL);
+	}
 	for (i = min; i <= max; i++)
 	{
 		p[j] = i;
-		j++
+		j++;
 	}
 	return (p);
 }
