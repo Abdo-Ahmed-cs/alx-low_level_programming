11fa230a660c0a09cd0a93645d8716d27474563c
abdelrahman ahmed saad <abdelrahman9789@gmail.com>
2023-03-20 15:39:16 -0700

    2-file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: 'prints the filte it was compiled from'
+ *
+ * Return: always success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return(0);
+}
