f229b377ff3b512a987b0eb295c48ba53cb64e10
abdelrahman ahmed saad <abdelrahman9789@gmail.com>
2023-03-21 15:17:25 -0700

    0.poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: name of the owner
+ *
+ * Description: 'structure dog that have all details of a dog'
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
