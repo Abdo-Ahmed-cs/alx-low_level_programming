1eea92e66fa9c9908d32416b91e737e934407cce
abdelrahman ahmed saad <abdelrahman9789@gmail.com>
2023-03-22 16:01:46 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,35 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * int_index - search for integer
+ * @array: array of integers
+ * @size: size of array
+ * @cmp: function pointer
+ *
+ * Return: return index of found number or -1 on failure
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (!array || !cmp)
+	{
+		return;
+	}
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
