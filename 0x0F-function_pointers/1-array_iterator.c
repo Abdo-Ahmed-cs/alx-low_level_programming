c3b2d16d81376210bf39f33dd8d412d303a232f0
abdelrahman ahmed saad <abdelrahman9789@gmail.com>
2023-03-22 15:41:52 -0700

    1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * array_iterator - prints every element of array
+ * @array: pointer array
+ * @size: array size
+ * @action: function to print array
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (!array || !size || !action)
+	{
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+
+}
