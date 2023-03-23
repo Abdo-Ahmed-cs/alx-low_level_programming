0174c60558d0eb2279dc5e356af8b2ed144b9968
abdelrahman ahmed saad <abdelrahman9789@gmail.com>
2023-03-23 02:43:14 -0700

    3-main.c 2

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,6 +15,7 @@ int main(int argc, char *argv[])
 {
 	int num1;
 	int num2;
+	char ch;
 	int (*f)(int, int);
 
 	if (argc != 4)
@@ -25,14 +26,18 @@ int main(int argc, char *argv[])
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
+
 	f = get_op_func(argv[2]);
+
 	if (!f)
 	{
 		printf("Error\n");
 		exit(99);	
 	}
+	
+	ch = *argv[2]
 
-	if ((argv[2] == '/' || argv[2] == '%') && num2 == 0)
+	if ((ch == '/' || ch == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
