aa6940283472de1355f72f9119a84f4e94d39218
Abdelrahman Megahed <abdelrahman9789@gmail.com>
2023-03-30 16:53:14 -0700

    4. free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: first node
+ */
+
+void free_list(list_t *head)
+{
+	list_t *free_me;
+	free_me = head;
+	if (head->next != NULL)
+	{
+		head = head->next;
+	}
+	free(free_me->str);
+	free(free_me->next);
+	free(free_me);
+}
