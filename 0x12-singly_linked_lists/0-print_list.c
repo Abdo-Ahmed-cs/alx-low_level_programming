2fb8a5d1d3b9d08ba0143c519e8b91ac0acefbd5
Abdelrahman Megahed <abdelrahman9789@gmail.com>
2023-03-30 15:40:58 -0700

    0.print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * print_list - print all the elemnts of a list
+ * @h: header to the list
+ * Return: the number of elements
+ */
+
+size_t print_list(const list_t *h)
+{
+	int i = 0, j = 1;
+
+	while (j)
+	{
+		if (h->str != NULL)
+		{
+			printf("[%d]%s\n", h->len, h->str);
+			i++;
+		} else
+		{
+			printf("[0] (nil)\n");
+			i++;
+		}
+		if (h->next == NULL)
+		{
+			j = 0;
+		}
+
+		h = h->next;
+	}
+	return (i);
+}
