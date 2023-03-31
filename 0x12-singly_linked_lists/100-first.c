268e6c2a33102492133c5d6f5375b2450af00a03
Abdelrahman Megahed <abdelrahman9789@gmail.com>
2023-03-30 17:18:39 -0700

    5. the hare and the tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * premain - run code before main
+ */
+
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("(A tortoise, having pretty good sense ");
+	printf(" of a hare's nature, challenges one to a race.)\n");
+	return (0);
+}
