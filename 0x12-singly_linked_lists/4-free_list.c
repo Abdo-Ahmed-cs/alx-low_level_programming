8fd82f5a3dc56c0b88beaf26132f5812c486c023
Abdelrahman Megahed <abdelrahman9789@gmail.com>
2023-03-30 17:08:59 -0700

    4.free list 6

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,25 +7,13 @@
 
 void free_list(list_t *head)
 {
-	list_t *free_me;
+	list_t *temp;
 
-	if (head == NULL)
+	while (head)
 	{
-		return;
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
 	}
-	free_me = head;
-
-	if (head->next != NULL)
-	{
-		head = head->next;
-	}
-	if (free_me->str != NULL)
-	{
-		free(free_me->str);
-	}
-	if (free_me->next != NULL)
-	{
-		free(free_me->next);
-	}
-	free(free_me);
 }
