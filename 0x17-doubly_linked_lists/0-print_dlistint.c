d451ba0ef8aaf478392bea297f87bda272eb434e
Abdelrahman Megahed <abdelrahman9789@gmail.com>
2023-05-20 16:34:37 -0700

    0. Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,21 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print a double linked list
+ * @h: header of the linked list
+ * Return: number of nodes
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count;
-
-	count = 0;
+	size_t i = 0;
 
 	if (h == NULL)
-		return (count);
-
-	while (h->prev != NULL)
-		h = h->prev;
-
-	while (h != NULL)
 	{
-		printf("%d\n", h->n);
-		count++;
-		h = h->next;
+		return (i);
 	}
 
-	return (count);
+	while(h != NULL)
+	{
+		i++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	
+	while (h)
+	{
+		h = h->prev;
+	}
+	return (i);
 }
