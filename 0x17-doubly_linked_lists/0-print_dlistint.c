4bf97f52c0c12b31599ef4aa04d4bed0853db3d6
Abdelrahman Megahed <abdelrahman9789@gmail.com>
2023-05-20 16:36:34 -0700

    0 print 2

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -15,16 +15,16 @@ size_t print_dlistint(const dlistint_t *h)
 		return (i);
 	}
 
+        while (h->prev)
+        {
+                h = h->prev;
+        }
+
 	while(h != NULL)
 	{
 		i++;
 		printf("%d\n", h->n);
 		h = h->next;
-	}
-	
-	while (h)
-	{
-		h = h->prev;
 	}
 	return (i);
 }
