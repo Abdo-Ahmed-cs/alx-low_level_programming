74bbb67c2377d80c169eea2b65dad37dd4d34025
Abdelrahman Megahed <abdelrahman9789@gmail.com>
2023-05-20 17:11:39 -0700

    2. Add node 4

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,17 +11,21 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
-
+	int num = n;
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 	{
 		return (NULL);
 	}
 
-	new->n = n;
-	(*head)->prev = new;
+	new->n = num;
 	new->next = *head;
 	new->prev = NULL;
+
+	if (*head != NULL)
+	{
+		(*head)->prev = new;
+	}
 	*head = new;
 
 	return (*head);
